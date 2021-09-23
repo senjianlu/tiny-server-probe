672799a691710880b00ad504d5a9c307d8b56993
Rabbir <m18018230731@163.com>
2021-09-23 19:45:13 +0800

    perf: 使用 Screen 后台执行探针

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ screen -S tiny-server-probe -X quit
 screen -wipe
 screen -dmS tiny-server-probe
 sleep 1s
-screen -S sub-2-proxy -X stuff 'uvicorn main:app --port 57191 --host 0.0.0.0\n'
+screen -S tiny-server-probe -X stuff 'uvicorn main:app --port 57191 --host 0.0.0.0\n'
