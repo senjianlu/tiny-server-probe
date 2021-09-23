9ab105fd0b5af0a59968a5476a0e262f7b2f281e
Rabbir <m18018230731@163.com>
2021-09-23 20:40:54 +0800

    fix: 修复 Screen 无法读取环境变量中 uvicorn 所在位置的问题

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ screen -S tiny-server-probe -X quit
 screen -wipe
 screen -dmS tiny-server-probe
 sleep 1s
-screen -S tiny-server-probe -X stuff '/usr/local/python3/bin/uvicorn main:app --port 57191 --host 0.0.0.0\n'
+screen -S tiny-server-probe -X stuff 'python3 -m uvicorn main:app --port 57191 --host 0.0.0.0\n'
